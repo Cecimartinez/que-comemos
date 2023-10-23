d58b3cac26aca5bc056d12ecb01d3cf45803810b
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-23 20:19:13 -0300

    Recipe card carousel

---
+++ que-comemos-app/src/data/data.js
@@ -0,0 +1,19 @@
+export const recetas = [
+  {
+    name: "Spaghetti al pesto",
+    imgSrc: "./pesto.svg",
+
+  },
+
+  {
+    name: "People Table",
+    imgSrc: "./food.svg",
+
+  },
+  {
+    name: "Delicate Macarons",
+    imgSrc: "./food.svg",
+
+  },
+
+];
--- que-comemos-app/src/pages/Home/Home.jsx
+++ que-comemos-app/src/pages/Home/Home.jsx
@@ -1,16 +1,56 @@
 import { NavigationBottom } from "../../components/NavigationBottom/NavigationBottom";
 import { SearchBar } from "../../components/SearchBard/SearchBard";
 import { Greeting } from "./components/Greeting/Greeting";
+import { recetas } from "../../data/data";
+import { StarIcons } from "./components/StarIcons/StarIcons";
 
 export const Home = () => {
   return (
     <div className="bg-white font-poppins flex flex-col justify-center items-baseline h-screen w-full">
-      <Greeting/>
+      <Greeting />
       <div className="w-full flex justify-center items-center">
-        <SearchBar/>
+        <SearchBar />
       </div>
-      <div className="bg-white px-6 py-16 my-24 w-full h-full"></div>
-      <NavigationBottom/>
+      <div className="bg-white px-6 py-8 mt-10 w-full h-full">
+        <section>
+          <h2 className="text-gray-400 text-xl tracking-widest font-poppins">Los más elegidos por nuestros Usuarios</h2>
+        </section>
+
+        <div className="carrousel-container w-full py-2 my-2 px-2 grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5">
+          {
+            recetas.map((receta) => (
+              <div
+                className="card-carrousel flex-col mx-auto w-11/12 sm:px-3 rounded-4xl flex shadow-lg  justify-center   bg-[#fff] "
+                draggable="false"
+              >
+                <a
+                  href={receta.link}
+                  target="_blank"
+                  rel="noopener noreferrer "
+                  className=""
+                >
+                  <div className=" flex  justify-center mx-10 my-4  py-2 px-10  rounded-full items-center ">
+                    <img src={receta.imgSrc} alt={receta.name} className=" w-full h-full" />
+                  </div>
+                </a>
+                <div className=" rounded-xl py-12  flex justify-center items-start px-7  flex-col h-20 text-gray-900">
+                  <h2 className="text-lg tracking-wider text-gray-500 mt-2 text-center font-medium font-poppins">
+                    {receta.name}
+                  </h2>
+
+                  <div className=" flex text-yellow-400 my-5 mx-1 space-x-2">
+                    {StarIcons.map((star, index) => (
+                      <div className=" " key={index}>{star}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+
+      </div>
+      <NavigationBottom />
     </div>
   );
 };
