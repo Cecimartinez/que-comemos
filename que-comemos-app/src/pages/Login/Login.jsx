13e79ce34d2d70a4b5d62466e878829df2dd2fa3
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-23 12:16:32 -0300

    Responsive Login Mobile

--- que-comemos-app/src/pages/Login/Login.jsx
+++ que-comemos-app/src/pages/Login/Login.jsx
@@ -1,21 +1,20 @@
 import { LoginForm } from "./components/LoginForm";
 
 export const Login = () => {
-
   return (
-    <div className="bg-white font-poppins flex flex-col justify-center items-baseline h-screen w-full ">
-      <div className='  bg-[#B4c170]  w-full h-full flex flex-col items-end justify-end rounded-b-5xl pr-6'>
-        <h1 className=' tracking-widest text-white mt-32 text-3xl font-medium '>
+    <div className="bg-white font-poppins flex flex-col justify-center items-baseline h-screen  w-full ">
+      <div className='bg-[#B4c170] w-full h-full flex flex-col items-end justify-end rounded-b-5xl pr-6 md:w-full   '>
+        <h1 className='tracking-widest text-white mt-16 md:mt-11 text-3xl font-medium'>
           Bienvenidos a
         </h1>
-        <h2 className='text-white mt-3 mb-20 text-3xl font-semibold tracking-widest ' > Qué Comemos</h2>
+        <h2 className='text-white mt-3 mb-20 md:mb-16 text-3xl font-semibold tracking-widest'>
+          Qué Comemos
+        </h2>
       </div>
 
-      <div className="bg-white px-6 py-16 my-24 w-full h-full">
-        <LoginForm/>
+      <div className="bg-white px-6 py-16 my-24 w-full h-full  md:justify-center md:px-60 md:my-8">
+        <LoginForm />
       </div>
     </div>
   );
 };
-
-
