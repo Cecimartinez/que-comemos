9a47b00bc9fa5933ddaa253dce4e55331f820de0
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-25 12:07:07 -0300

    RoutePaths and pages updated

---
+++ que-comemos-app/src/routes/utils/routePaths.js
@@ -0,0 +1,12 @@
+export const ROUTE_PATHS = {
+  HOME: "/",
+  LOGIN: "login",
+  SIGNIN:"sign-in",
+  USERPROFILE:"user-profile",
+  SAVEDRECIPES:"saved-recipes",
+  RECIPES:"recipes",
+  INGREDIENTS:"Ingredients",
+  STEPS:"steps",
+  
+
+};
--- que-comemos-app/src/routes/utils/routePaths.jsx
+++
@@ -1,6 +0,0 @@
-export const ROUTE_PATHS = {
-  HOME: "/",
-  LOGIN: "login",
-  SIGNIN:"sign-in",
-  USERPROFILE:"user-profile"
-};
