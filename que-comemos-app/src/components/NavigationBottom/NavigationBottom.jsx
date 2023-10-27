5100755b8d2726a19b0bb2ec4dee7ea5372ffcf1
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-27 10:10:06 -0300

    NavigationBottom Updated

--- que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
+++ que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
@@ -3,45 +3,42 @@ import { Link } from 'react-router-dom';
 export const NavigationBottom = () => {
   return (
     <div className="fixed bottom-0 z-50 w-full -translate-x-1/2 bg-white border-t border-gray-200 left-1/2">
-      <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
+      <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
         <Link to="/" data-tooltip-target="tooltip-home" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-          </svg>
-          <span className="sr-only">Home</span>
-        </Link>
+
+          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
+            Home
+          </span>        </Link>
         <div id="tooltip-home" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
           Home
           <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
 
-        <Link  to="/saved-recipes" data-tooltip-target="tooltip-bookmark" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
-            <path d="M13 20a1 1 0 0 1-.64-.231L7 15.3l-5.36 4.469A1 1 0 0 1 0 19V2a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v17a1 1 0 0 1-1 1Z" />
-          </svg>
-          <span className="sr-only">Bookmark</span>
+        <Link to="/saved-recipes" data-tooltip-target="tooltip-bookmark" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
+          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
+            bookmark
+          </span>
         </Link>
         <div id="tooltip-bookmark" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
           Bookmark
           <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
 
-        {/* <Link data-tooltip-target="tooltip-search" type="button" className="inline-flex flex-col items-center justify-center p-4 hover-bg-gray-50 ">
-          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-          </svg>
-          <span className="sr-only">Search</span>
+        <Link to="/" data-tooltip-target="tooltip-search" type="button" className="inline-flex flex-col items-center justify-center p-4 hover-bg-gray-50 ">
+          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
+            search
+          </span>
         </Link>
         <div id="tooltip-search" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
           Search
           <div className="tooltip-arrow" data-popper-arrow></div>
-        </div> */}
+        </div>
 
-        <Link  to="/user-profile" data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5m0-4.5a2.25 2.25 0 0 0 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
-          </svg>
-          <span className="sr-only">Settings</span>
+        <Link to="/user-profile" data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
+
+          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
+            person
+          </span>
         </Link>
         <div id="tooltip-settings" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
           Settings
