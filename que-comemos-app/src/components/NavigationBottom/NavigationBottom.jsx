79d070f3e346a62382fd6091018ead1988d5d4d9
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-27 11:09:59 -0300

    Global navigation botton component, refactored

--- que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
+++ que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
@@ -1,49 +1,13 @@
-import { Link } from 'react-router-dom';
+import { NavigationItem } from "../NavigationItem/NavigationItem";
 
 export const NavigationBottom = () => {
   return (
-    <div className="fixed bottom-0 z-50 w-full -translate-x-1/2 bg-white border-t border-gray-200 left-1/2">
-      <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
-        <Link to="/" data-tooltip-target="tooltip-home" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-
-          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
-            Home
-          </span>        </Link>
-        <div id="tooltip-home" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-          Home
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-
-        <Link to="/saved-recipes" data-tooltip-target="tooltip-bookmark" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
-            bookmark
-          </span>
-        </Link>
-        <div id="tooltip-bookmark" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-          Bookmark
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-
-        <Link to="/" data-tooltip-target="tooltip-search" type="button" className="inline-flex flex-col items-center justify-center p-4 hover-bg-gray-50 ">
-          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
-            search
-          </span>
-        </Link>
-        <div id="tooltip-search" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-          Search
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-
-        <Link to="/user-profile" data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 ">
-
-          <span className="material-symbols-outlined w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 hover:text-[#B4c170] ">
-            person
-          </span>
-        </Link>
-        <div id="tooltip-settings" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-          Settings
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
+    <div className="fixed bottom-0 py-4  z-50 w-full bg-white border-t border-gray-200 left-9">
+      <div className="grid h-full max-w-lg grid-cols-4 mx-auto items-center justify-center">
+        <NavigationItem to="/"  icon="Home" />
+        <NavigationItem to="/saved-recipes"  icon="bookmark" />
+        <NavigationItem to="/search"  icon="search" />
+        <NavigationItem to="/user-profile"  icon="person" />
       </div>
     </div>
   );
