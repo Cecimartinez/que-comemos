8b0dccca555100e73f3b3003d6dc5676690ab939
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-27 11:20:13 -0300

    SearchBard updated

--- que-comemos-app/src/components/SearchBard/SearchBard.jsx
+++ que-comemos-app/src/components/SearchBard/SearchBard.jsx
@@ -3,7 +3,7 @@ export const SearchBar = () => {
     <div className="absolute  ">
       <input
         type="search"
-        className="input text-base transition-all duration-500 ease-in-out bg-white shadow-xl border py-9 items-center px-20 justify-center rounded-5xl focus:border-2 focus:border-[#abb867] focus:bg-white"
+        className="input text-lg transition-all duration-500 ease-in-out bg-white shadow-xl border py-10  items-center px-16 font-medium justify-center rounded-5xl focus:border-2 focus:border-[#abb867] focus:bg-white"
         placeholder="Qué comemos hoy?"
       />
       <button className="text-gray-400 absolute right-4 top-2/4 transform -translate-y-2/4 px-5 py-12">
