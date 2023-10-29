96ee8130d3a86db62c285e04488a32519078a312
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-29 02:52:12 -0300

    CardRecipes Updated

--- que-comemos-app/src/components/CardRecipes/CardRecipes.jsx
+++ que-comemos-app/src/components/CardRecipes/CardRecipes.jsx
@@ -1,21 +1,39 @@
+
 export const CardRecipes = () => {
   return (
-
     <div className="flex max-w-md overflow-hidden bg-white rounded-xl shadow-lg mx-5 my-3">
-      <div className="w-1/3 h-full bg-cover bg-landscape ">
-        <img className="h-full w-full rounded-xl" src="././cookie.webp"/>
+      <div className="w-1/3 h-full bg-cover bg-landscape">
+        <img className="h-full w-full rounded-xl" src="././cookie.webp" alt="Cookie Image" />
       </div>
-      <div className="w-2/3 py-10 px-5">
-        <h1 className="text-lg font-bold text-neutral-900-900">
-          Cookie Chips
-        </h1>
-        <p className="mt-2 text-sm text-gray-600">
-          El poco horneado hace que las sean
-          más crujientes.
+      <div className="w-2/3 py-8 px-5">
+        <h1 className="text-lg font-medium text-neutral-900">Cookie Chips</h1>
+        <p className="mt-2 text-sm text-neutral-500">
+          El poco horneado hace que sean más crujientes.
         </p>
+        <div className="flex justify-center items-center mt-2 gap-6 text-sm text-neutral-600">
 
+          <div className="flex justify-center items-center ">
+            <span className="mr-2 material-symbols-outlined "> timer
+            </span>
+            <p className=""></p> 20 min
+          </div>
+
+          <div className="flex justify-center items-center ">
+            <span className="material-symbols-outlined">star
+            </span>
+            <p className="py-2 px-1"></p> 4.2
+          </div>
+
+
+          <div className="flex justify-center items-center ">
+            <span className="mr-2 material-symbols-outlined"> bookmark
+            </span>
+
+          </div>
+
+        </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
