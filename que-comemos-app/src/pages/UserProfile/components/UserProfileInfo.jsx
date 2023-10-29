96b264091cb7bbd78012881acebcf11ac3050861
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-29 02:08:45 -0300

    UserProfileInfo component updated

--- que-comemos-app/src/pages/UserProfile/components/UserProfileInfo.jsx
+++ que-comemos-app/src/pages/UserProfile/components/UserProfileInfo.jsx
@@ -2,8 +2,8 @@ import { Modal } from "../../../components/ui/Modal";
 
 export const UserProfileInfo = () => {
   return (
-    <div className="px-10 py-20 flex justify-center items-center w-full">
-      <div className="w-full max-w-md border-neutral-200 border bg-white shadow-xl py-20 rounded-xl px-5">
+    <div className="px-10 pt-16  flex justify-center items-center w-full">
+      <div className="w-full max-w-md border-neutral-200 border bg-white shadow-xl  py-20 rounded-xl px-5">
         <div className="flex flex-col items-center">
           <div className="avatar online my-5 mx-3">
             <div className="w-24 h-24 rounded-full border">
