ec678e333ec6e84d36b544deea0c40ca4ce0f576
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-29 16:19:11 -0300

    NavigationBottom component, updated

--- que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
+++ que-comemos-app/src/components/NavigationBottom/NavigationBottom.jsx
@@ -2,8 +2,8 @@ import { NavigationItem } from "../NavigationItem/NavigationItem";
 
 export const NavigationBottom = () => {
   return (
-    <div className="fixed bottom-0 py-4  z-50 w-full bg-white border-t border-gray-200 left-9">
-      <div className="grid h-full max-w-lg grid-cols-4 mx-auto items-center justify-center">
+    <div className="fixed bottom-0 py-4  z-50 w-full bg-white border-t border-gray-200  justify-center items-center">
+      <div className=" h-full max-w-lg gap-20 mx-auto  flex items-center justify-center">
         <NavigationItem to="/"  icon="Home" />
         <NavigationItem to="/saved-recipes"  icon="bookmark" />
         <NavigationItem to="/recipe-search"  icon="search" />
