98c9b93ef4394f4c867c243d90bf3edebbd8643d
Ceci Martínez <98435962+Cecimartinez@users.noreply.github.com>
2023-11-01 15:52:04 -0300

    home page responsive

--- que-comemos-app/src/components/SearchBard/SearchBard.jsx
+++ que-comemos-app/src/components/SearchBard/SearchBard.jsx
@@ -3,11 +3,11 @@ export const SearchBar = () => {
     <div className="absolute  ">
       <input
         type="search"
-        className="input text-lg transition-all duration-500 ease-in-out bg-white shadow-xl border py-10  items-center px-16 font-medium justify-center rounded-5xl focus:border-2 focus:border-[#abb867] focus:bg-white"
+        className="input text-lg lg:text-3xl transition-all duration-500 ease-in-out bg-white shadow-xl border py-10  lg:py-14   items-center px-16 font-medium justify-center rounded-5xl focus:border-2 focus:border-[#abb867] focus:bg-white"
         placeholder="Qué comemos hoy?"
       />
-      <button className="text-gray-400 absolute right-4 top-2/4 transform -translate-y-2/4 px-5 py-12">
-        <span className="material-symbols-outlined">search</span>
+      <button className="text-gray-400 absolute right-4 top-1/2 transform -translate-y-2/4 px-3 ">
+        <span className="material-symbols-outlined lg:text-3xl">search</span>
       </button>
     </div>
   );
--- que-comemos-app/src/pages/Home/components/Greeting/Greeting.jsx
+++ que-comemos-app/src/pages/Home/components/Greeting/Greeting.jsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../../../../components/Sidebar/Sidebar";
 
 export const Greeting = () => {
   return (
-    <header className='bg-[#B4c170] mb-2 w-full h-full flex flex-col items-start  gap-8 rounded-b-5xl'>
+    <header className='bg-[#B4c170] mb-2 w-full h-2/3 flex flex-col px-7  items-center lg:h-1/3 justify-start rounded-b-5xl'>
       <div className=" w-full flex justify-between px-5 py-5 items-baseline">
         <Sidebar />
         <section className=" text-center ">
@@ -11,11 +11,11 @@ export const Greeting = () => {
         </section>
       </div>
 
-      <div className="px-5">
-        <h1 className='tracking-wide text-white text-3xl font-medium'>
+      <div className="px-5 flex flex-col w-full justify-center items-start h-full lg:my-10 pb-10">
+        <h1 className='tracking-wide text-white lg:text-6xl  text-3xl  font-medium'>
           Buenos Días
         </h1>
-        <h2 className='text-white  text-3xl font-medium tracking-wide'>
+        <h2 className='text-white lg:text-6xl  text-3xl font-medium tracking-wide'>
           Emma
         </h2>
       </div>
