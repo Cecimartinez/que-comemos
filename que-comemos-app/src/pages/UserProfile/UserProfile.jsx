5738c940d432e33f353673bdd5109336ce70ef9b
Ceci Martínez <98435962+Cecimartinez@users.noreply.github.com>
2023-11-21 22:26:06 -0300

    Update Header component, UserProfile page

--- que-comemos-app/src/pages/UserProfile/UserProfile.jsx
+++ que-comemos-app/src/pages/UserProfile/UserProfile.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from 'react'
-import { Header } from "../../components/Header/Header";
+import { useContext, useEffect } from 'react'
 import { NavigationBottom } from "../../components/NavigationBottom/NavigationBottom";
 import { UserProfileInfo } from "./components/UserProfileInfo";
 import { useNavigate } from "react-router-dom";
 import UsuarioContext from '../../context/usuarios/usuarioContext';
+import { HeaderUser } from './components/HeaderUser';
 
 export const UserProfile = () => {
 
@@ -17,7 +17,7 @@ export const UserProfile = () => {
 
   return (
     <div className="bg-white font-poppins flex flex-col justify-center items-baseline min-h-screen w-full">
-      <Header page="Datos personales"/>
+      <HeaderUser page="Datos personales"/>
       <UserProfileInfo/>
       <NavigationBottom />
     </div>
---
+++ que-comemos-app/src/pages/UserProfile/components/HeaderUser.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export const HeaderUser = ({page}) => {
+  return (
+    <div className="bg-[#B4C170] text-white w-full top-0 fixed py-5  px-4 flex items-start justify-between">
+      <Link to="/" className="text-lg inline-flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+        </svg>
+        Home
+      </Link>
+      <h1 className="text-xl font-medium">{page}</h1>
+    </div>
+  );
+};
