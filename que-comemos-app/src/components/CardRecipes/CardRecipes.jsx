23fdc60e494ae80042aa50f1860ca5991c73aefa
Ceci Martínez <98435962+Cecimartinez@users.noreply.github.com>
2023-11-21 22:51:37 -0300

    CardRecipes component and SaveRecipes page, updated

--- que-comemos-app/src/components/CardRecipes/CardRecipes.jsx
+++ que-comemos-app/src/components/CardRecipes/CardRecipes.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 import useGetById from '../../services/recetas/useGetById'
 
 export const CardRecipes = ({ recetaId, isSaved }) => {
@@ -8,9 +7,7 @@ export const CardRecipes = ({ recetaId, isSaved }) => {
 
   if (!data) {
     return (
-      <div>
-        Cargando
-      </div>
+      <span className="loading loading-ring loading-md"></span>
     )
   }
 
@@ -18,21 +15,19 @@ export const CardRecipes = ({ recetaId, isSaved }) => {
 
   return (
     <Link to={`/ingredientes/${recetaId}`}>
-      <div className="flex max-w-md  overflow-hidden  bg-white border border-neutral-300 rounded-xl shadow-xl my-2 mx-4 py-4 px-4">
+      <div className="flex max-w-md  overflow-hidden  bg-white border border-neutral-200 rounded-xl shadow-xl my-2 mx-4 py-4 px-4">
         <div className="w-1/3 h-full  bg-cover bg-landscape flex justify-center items-center ">
           <img className="h-full w-full  rounded-xl" src={`https://quecomemos-servidor-production.up.railway.app/api/images/${recetaId}`} alt="Cookie Image" />
         </div>
         <div className="w-2/3 px-5 flex flex-col justify-center items-start">
           <h1 className="text-lg font-medium text-neutral-900">{nombre}</h1>
-          {/* <p className="mt-1 text-sm text-neutral-500">
-            El poco horneado hace que sean más crujientes.
-          </p> */}
+
           <div className="flex justify-center items-center mt-4 gap-6 text-xs text-neutral-600">
 
             <div className="flex justify-center items-center ">
               <span className="mr-2 material-symbols-outlined "> timer
               </span>
-              <p className=""></p> {tiempoPreparacion}'
+              <p className=""></p> {tiempoPreparacion}
             </div>
 
             <div className="flex justify-center items-center ">
@@ -43,9 +38,8 @@ export const CardRecipes = ({ recetaId, isSaved }) => {
             {
               isSaved && (
                 <div className="flex justify-center items-center ">
-                  <span className={`mr-2 material-symbols-outlined text-yellow-500`}> bookmark
+                  <span className={`mr-2 material-symbols-outlined text-[#B4C170]`}> bookmark
                   </span>
-
                 </div>
               )
             }
--- que-comemos-app/src/pages/SavedRecipes/SavedRecipes.jsx
+++ que-comemos-app/src/pages/SavedRecipes/SavedRecipes.jsx
@@ -10,20 +10,15 @@ export const SavedRecipes = () => {
   return (
 
     <div className="bg-[#fff] font-poppins flex flex-col justify-between items-center h-full h-min:h:screen w-full">
-
       <Header page="Recetas Guardadas" />
-
       <div className="pb-20 mt-10 justify-center  items-center h-full grid  md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5">
 
         {
           recetasGuardadas?.map(recetaId => (
-            <CardRecipes recetaId={recetaId} isSaved={true}/>
+            <CardRecipes key={recetaId
+            } recetaId={recetaId} isSaved={true} />
           ))
         }
-        {/* <CardRecipes />
-        <CardRecipes />
-        <CardRecipes />
-        <CardRecipes /> */}
       </div>
 
       <NavigationBottom />
